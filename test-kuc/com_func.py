d12d0a11214da048adb8e58795e33e9918300a27
kuc-arc-f <naka@kuc-arc-f.com>
2017-10-27 10:18:02 +0900

    new ,10-27a1
    Signed-off-by: kuc-arc-f <naka@kuc-arc-f.com>

---
+++ test-kuc/com_func.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+# 日本語
+from urllib2 import urlopen
+import webapp2
+import json
+import os
+import datetime
+
+#define
+mDnsName="http://your-dns.com"
+
+#com_func
+class funcClass:
+	def __init__(self):
+		print ""
+	
+	def get_heiNum(self, v1 ,v2):
+		ret=0.0
+		fv = float(v1) + float(v2)
+		ret= fv / 2
+		return ret
+		
+	def get_message(self):
+		sURL =mDnsName +"/tst/ghome/dat/device1.json"
+		result = json.load(urlopen(sURL))
+		text = "ok, リビングの温度は、" +str(result["temperature"])+ " 度です。"
+		sURL2 =mDnsName +"/tst/ghome/dat/device4.json"
+		result2 = json.load(urlopen(sURL2 ))
+		sBuf2 ="玄関の温度は、" + str(result2["temperature"]) +"度です。"
+		text += sBuf2
+		fAva= self.get_heiNum(result["temperature"] , result2["temperature"])
+#		fAva= self.get_heiNum(1.0  , 2.0)
+		sBuf3="センサー値の平均は、" + str(fAva) + "度です。"
+		text +=sBuf3
+		#time
+		tmNow = datetime.datetime.now()
+		tmNow= tmNow + datetime.timedelta(hours=9)
+		sHH = tmNow.strftime("%H")
+		sMM = tmNow.strftime("%M")
+		sTime = " 現在時刻は、" + sHH+"時"+ sMM+ "分です。"
+		text += sTime
+		return text
+		
+
+
+
+
